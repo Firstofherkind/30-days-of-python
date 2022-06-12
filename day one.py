810c5d338aa5c144b185ab87afdc54e851393af9
Your Name <egwujennifer@yahoo.com>
2022-06-12 16:18:47 +0100

    Day one

---
+++ day one.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 11 14:54:16 2022
+
+@author: DELL
+"""
+
+#This is an introduction to python. It's day one!
+
+#carrying out simple arithmetic.S
+
+print(2 + 3) #add two numbers together
+print(2 - 3) #subtracts a number from another
+print(5 ** 5) #returns the squared of a number
+print(4 / 2) #floor division returns division resultssss in decimal
+print(4 // 2) #returns division in integers
+print(5 % 2) #returns the remainder from division
+print(3 * 3) #multiplies a number by another number
+
+
+#data types
+#data can be stored in different forms.we also use the type keyword tocheck data types. 
+print(type(2)) #integer
+print(type('Jennifer')) #string
+print(type(4 + 3j)) #complex number
+print(type({'Egwu': 'Jennifer', 'Ojo':'Johnson'})) #dictionary
+print(type((1, 2, 'three'))) #tuple
+print(type({1, 2, 'hi'})) #set
+print(type(2.5)) #float
+print(type([1, 3, 'Jennifer'])) #list
