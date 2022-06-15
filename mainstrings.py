723d384390b22f91d860b6082d7e8a13d8173815
Egwu Jennifer <egwujennifer@yahoo.com>
2022-06-15 23:34:37 +0100

    Add files via upload

---
+++ mainstrings.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 15 14:11:46 2022
+
+@author: DELL
+"""
+
+
+
+print('Thirty ' + 'Days ' +'of ' + 'Python')
+
+company = 'Coding for all'
+print(company)
+print(len(company))
+print(company.upper())
+print('Lower:', company.lower())
+print('Capital:', company.capitalize())
+print('title:', company.title())
+print('Swap:', company.swapcase())
+print('First word:', company[0:5])
+
+print('find:', company.find('Coding'))
+print('Find:', company.rfind('coding'))
+
+print('Name\tAge\tCountry\nJennifer\t24\tNigeria' )
